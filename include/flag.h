20339e4d04cd75a25f3af3c4b16ec4a5b637ff8e
Dr.Xiao <zxc71230@gmail.com>
2020-11-29 16:59:18 +0800

    Revise send and receive

--- include/flag.h
+++ include/flag.h
@@ -4,15 +4,15 @@
 #define FG_ARP_SEND_REQUEST 0
 
 #define DEBUG_PHYSICAL 0
-#define DEBUG_ETH 1
+#define DEBUG_ETH 0
 
-#define DEBUG_IP 1
+#define DEBUG_IP 0
 
 #define DEBUG_ARP 0
 #define DEBUG_ARP_REQUEST 0
 #define DEBUG_ARP_REPLY 0
 #define DEBUG_ARP_DUMP 0
 
-#define DEBUG_TCP 1
+#define DEBUG_TCP 0
 
 #endif /* __FLAG_H__ */
